667e82a33f57160e81a9f93b6e8f585c68878514
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2021-08-27 00:14:26 -0700

    Version 1

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Link, LinkProps} from "../../components/Link/Link";
 import icons from './icons';
+import profile_img from './profile_img.png';
 import './App.css';
 
 const links: LinkProps[] = [
@@ -14,15 +15,59 @@ const links: LinkProps[] = [
         }
     },
     {
-        url: 'https://joserodriguezrivas.com',
-        text: "Portfolio"
-    },
-    {
         url: "https://twitter.com/LeRealJoseRivas",
         text: "Twitter",
         icon: {
             src: icons.twitter,
             alt: "Twitter Logo"
+        }
+    },
+    {
+        url: "https://www.twitch.tv/throopie",
+        text: "Twitch",
+        icon: {
+            src: icons.twitch,
+            alt: "Twitch Logo Glitch"
+        }
+    },
+    {
+        url: "https://www.tiktok.com/@joserivas896?lang=en",
+        text: "TikTok",
+        icon: {
+            src: icons.tiktok,
+            alt: "TikTok Logo"
+        }
+    },
+    {
+        url: "https://www.instagram.com/joserodriguezrivas_tcg/",
+        text: "Personal Instagram",
+        icon: {
+            src: icons.instagram,
+            alt: "Instagram Logo"
+        }
+    },
+    {
+        url: "https://www.instagram.com/tinycountrygames/",
+        text: "TCG Instagram",
+        icon: {
+            src: icons.instagram,
+            alt: "Instagram Logo"
+        }
+    },
+    {
+        url: 'https://github.com/JoseRivas1998',
+        text: "GitHub",
+        icon: {
+            src: icons.github,
+            alt: "GitHub Icon"
+        }
+    },
+    {
+        url: 'https://joserodriguezrivas.com',
+        text: "Portfolio",
+        icon: {
+            src: icons.tcg,
+            alt: "Tiny Country Games Logo"
         }
     }
 ];
@@ -33,6 +78,10 @@ function App() {
     );
     return (
         <>
+            <div className={"ProfilePicture"}>
+                <img src={profile_img} alt={"Digital Drawing of Jose"}/>
+            </div>
+            <h1>Jos&eacute; Rodriguez Rivas</h1>
             {allLinks}
         </>
     );
--- src/containers/App/icons/index.ts
+++ src/containers/App/icons/index.ts
@@ -1,8 +1,12 @@
 import tcg from './tcg.png';
 import twitter from './twitter.png';
+import twitch from './twitch.png';
+import instagram from './instagram.png';
+import tiktok from './tiktok.png';
+import github from './github.png';
 
 const images = {
-    tcg, twitter
+    tcg, twitter, twitch, instagram, tiktok, github
 };
 
 export default images;
