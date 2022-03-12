66a39d7da71c51eddc2c9c68fc8d91c7f365f679
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2022-03-12 11:44:58 -0800

    JRR-4 Data driven links

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,78 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 import {Link, LinkProps} from "../../components/Link/Link";
-import icons from './icons';
 import profile_img from './profile_img.png';
 import './App.css';
 
-const links: LinkProps[] = [
-    {
-        url: "https://tinycountrygames.com",
-        text: "Tiny Country Games",
-        icon: {
-            src: icons.tcg,
-            alt: "Tiny Country Games Logo"
-        }
-    },
-    {
-        url: "https://twitter.com/LeRealJoseRivas",
-        text: "Twitter",
-        icon: {
-            src: icons.twitter,
-            alt: "Twitter Logo"
-        }
-    },
-    {
-        url: "https://www.twitch.tv/throopie",
-        text: "Twitch",
-        icon: {
-            src: icons.twitch,
-            alt: "Twitch Logo Glitch"
-        }
-    },
-    {
-        url: "https://www.tiktok.com/@joserivas896?lang=en",
-        text: "TikTok",
-        icon: {
-            src: icons.tiktok,
-            alt: "TikTok Logo"
-        }
-    },
-    {
-        url: "https://www.instagram.com/joserodriguezrivas_tcg/",
-        text: "Personal Instagram",
-        icon: {
-            src: icons.instagram,
-            alt: "Instagram Logo"
-        }
-    },
-    {
-        url: "https://www.instagram.com/tinycountrygames/",
-        text: "TCG Instagram",
-        icon: {
-            src: icons.instagram,
-            alt: "Instagram Logo"
-        }
-    },
-    {
-        url: 'https://github.com/JoseRivas1998',
-        text: "GitHub",
-        icon: {
-            src: icons.github,
-            alt: "GitHub Icon"
-        }
-    },
-    {
-        url: 'https://joserodriguezrivas.com',
-        text: "Portfolio",
-        icon: {
-            src: icons.tcg,
-            alt: "Tiny Country Games Logo"
-        }
-    }
-];
-
 function App() {
+
+    const [links, set_links] = useState<LinkProps[]>([]);
+
+    const load_links = async () => {
+        const get_result = await axios.get("/portfolio/links");
+        return get_result.data.map((db_link: any) => {
+            const link : LinkProps = {
+                ...db_link
+            };
+            if (db_link.icon) {
+                link.icon = {
+                    ...db_link.icon,
+                    src: `/icons/${db_link.icon.src}.png`
+                }
+            }
+            return link;
+        });
+    };
+
+    useEffect(() => {
+        load_links().then(set_links);
+    }, []);
+
     const allLinks = links.map(
         (link, index) => <Link url={link.url} text={link.text} icon={link.icon} key={index}/>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import App from './containers/App/App';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_ROOT;
 
 ReactDOM.render(
   <React.StrictMode>
