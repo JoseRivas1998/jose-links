be94ae0de401e9ba4a231e0c617920db488b149e
Jose Rodriguez Rivas <joserivas823@gmail.com>
2024-09-08 17:41:25 -0700

    Create next app

--- src/components/Link/LinkIcon/LinkIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {ImageProps} from "../../../common/Types";
-import styles from './LinkIcon.module.css';
-
-export const LinkIcon = (props: ImageProps) => (
-    <img src={props.src} alt={props.alt} className={styles.LinkIcon}/>
-);
